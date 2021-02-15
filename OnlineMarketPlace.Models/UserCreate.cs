5aa6d2c7b87d5c8bcc65bfff054534380a080640
Parker Anderson <parker.anderson0810@outlook.com>
2021-02-15 16:15:16 -0500

    UserCreate

---
+++ OnlineMarketPlace.Models/UserCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarketPlace.Models
+{
+    public class UserCreate
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string UserRole { get; set; }
+
+    }
+}
