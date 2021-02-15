35101cb376bab1340acea999e2f4f71f229a717d
Parker Anderson <parker.anderson0810@outlook.com>
2021-02-15 16:10:10 -0500

    UserListItem

---
+++ OnlineMarketPlace.Models/UserListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarketPlace.Models
+{
+    public class UserListItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        [Display(Name="Joined")]
+        public DateTime DateJoined { get; set; }
+        public string UserRole { get; set; }
+
+    }
+}
