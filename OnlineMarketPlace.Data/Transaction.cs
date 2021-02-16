9be0a77451df299d33e10841ad02fe1247347aa0
MaryCaroleScannnell <73565044+MaryCaroleScannnell@users.noreply.github.com>
2021-02-16 18:10:02 -0500

    MC Branch

--- OnlineMarketPlace.Data/Transaction.cs
+++ OnlineMarketPlace.Data/Transaction.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineMarketPlace.Data
 {
+    public enum ShoppingCategory
+    {
+        Clothing = 1,
+        Electronics,
+        Home,
+        Kids,
+        Outdoor,
+        Art
+    }
     public class Transaction
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public TimeSpan HowLongOnMarket { get; set; }
+        public ShoppingCategory Category { get; set; }
+        public string Description { get; set; }
+        
+        //[ForeignKey(nameof(User)))]
+        // public int UserId { get; set; }
+        //public virtual User UserId {get; set;}
+
+
     }
 }
---
+++ OnlineMarketPlace.Models/TransactionCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarketPlace.Models
+{
+    public class TransactionCreate
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
