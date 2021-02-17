1941b4796dec49fb41ca0cb4551f8e7dfdabf6c2
MaryCaroleScannnell <73565044+MaryCaroleScannnell@users.noreply.github.com>
2021-02-16 19:20:02 -0500

    fixing MC mistakes

--- OnlineMarketPlace.Data/Product.cs
+++ OnlineMarketPlace.Data/Product.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineMarketPlace.Data
 {
+    public enum ShoppingCategory
+    {
+        Clothing = 1,
+        Electronics,
+        Home,
+        Kids,
+        Outdoor,
+        Art
+    }
     public class Product
     {
+
     }
 }
