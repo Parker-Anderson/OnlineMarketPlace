c6a1be492a64122865234a773d508855338cde01
Parker Anderson <parker.anderson0810@outlook.com>
2021-02-18 00:35:13 -0500

    Created UserController.cs, GET and POST methods for User

---
+++ OnlineMarketPlace.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using OnlineMarketPlace.Models;
+using OnlineMarketPlace.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineMarketPlace.WebAPI.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            UserService userService = CreateUserService();
+            var users = userService.GetUsers();
+            return Ok(users);
+        }
+        public IHttpActionResult Post(UserCreate user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateUserService();
+            if (!service.CreateUser(user))
+                return InternalServerError();
+            return Ok();
+        }
+        private UserService CreateUserService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var userService = new UserService(userId);
+            return userService;
+        }
+        
+        
+    }
+}
