cddf60259cee6c13f0d33e70cb1a6e10f641a615
Seth-Thomas <Seth.b.thomas8@gmail.com>
2021-03-06 09:41:24 -0500

    last push?

--- OnlineMarketPlace.Data/IdentityModels.cs
+++ OnlineMarketPlace.Data/IdentityModels.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using static OnlineMarketPlace.Data.User;
 
 namespace OnlineMarketPlace.Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        //public UserRole Buyer { get; set; }
+        //public UserRole Seller { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
